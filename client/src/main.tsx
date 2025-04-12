7aad610756b9952414695ce49f32511f65380f56
Kidus Sebsibe <sonofmulugeta@gmail.com>
2025-04-12 14:00:48 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import { AuthProvider } from "./contexts/AuthContext";
 
-// Simplified root with just QueryClientProvider to avoid circular dependencies
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );
